38a4771ace20425f735bc6b7ad9c98290d1b7e55
Eugenia-P <eugenia.protasevich@yandex.by>
2019-10-11 16:27:09 +0300

    feat: my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function zeros(expression) {
-  // your solution
+  let fives = 0; 
+  let twos = 0; 
+  let fact = expression.split('*');
+
+  fact.forEach(el => {
+    let exclMark = (el.indexOf('!!') === -1) ? 1 : 2; 
+    el = parseInt(el); 
+
+    while (el > 0) {
+      let temp = el; 
+      while (temp % 5 === 0) {
+        fives++; 
+        temp /= 5; 
+      } 
+
+      temp = el; 
+      while (temp % 2 === 0) {
+        twos++; 
+        temp /= 5; 
+      }
+
+      el -= exclMark; 
+    }
+  }); 
+  
+  return Math.min(fives, twos); 
 }
